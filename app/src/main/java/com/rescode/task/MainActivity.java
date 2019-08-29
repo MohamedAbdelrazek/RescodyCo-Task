8f9628496ad0fb60f6d4186aefd64b76119cdc06
MohamedAbdelrazek <muhamedrabi3@gmail.com>
2019-08-29 21:37:38 +0200

    Finishing UI Desdign

---
+++ app/src/main/java/com/rescode/task/MainActivity.java
@@ -0,0 +1,21 @@
+package com.rescode.task;
+
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().getDecorView().setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+        setContentView(R.layout.activity_main);
+    }
+
+}
+
+
